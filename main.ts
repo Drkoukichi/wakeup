373be5bfca76d79918477dab441efb959e7d67df
koukichi-net <68998364+koukichi-net@users.noreply.github.com>
2021-08-28 17:01:28 +0900

    第一回までのまとめ

--- main.ts
+++ main.ts
@@ -1,20 +1,23 @@
-function check () {
+function check () //現在の本体の角度をもとに寝ているかの判断を行う
+{
     let p: number
-let stl : number
-let sth : number
-let tf: boolean
-stl = 0
+    let stl : number
+    let sth : number
+    let tf: boolean
+    stl = 0
     sth = 110
     p = input.rotation(Rotation.Pitch)
     tf = p < stl || p > sth
     return tf
 }
 
-function sleep () {
+function sleep ()　//寝ていた場合の処理 
+{
     led.plot(2, 2)
 }
 
-function desleep () {
+function desleep ()　//寝ていなかった場合、もしくは起床した場合 
+{
     led.unplot(2, 2)
 }
 
